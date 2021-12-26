c8640681135e290d1700aee7df7374bcdb1b34c5
VadimVvs <vadimzno18@gmail.com>
2021-12-26 13:38:46 +0200

    fix for heroku

--- src/app/_services/item.service.ts
+++ src/app/_services/item.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Item } from "../_models/item.model";
-import { environment } from "../../environments/environment";
 import { map } from "rxjs/operators";
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://flowers-project.herokuapp.com/api'
 };
