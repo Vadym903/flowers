5eab5abd4b000899eb9211189e89196277217e0b
VadimVvs <vadimzno18@gmail.com>
2021-12-27 22:40:27 +0200

    added orders displaying for user's home page

--- src/app/_components/shared/login/login.component.ts
+++ src/app/_components/shared/login/login.component.ts
@@ -74,7 +74,7 @@ export class LoginComponent implements OnInit {
         this.router.navigateByUrl('/items');
       });
     }, error => {
-      alert(error[0]);
+      alert(JSON.stringify(error));
     });
 
   }
